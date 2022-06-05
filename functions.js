64ff3dde19abf9b886c89e64b76bfa1b65a4422a
ArtemSmotritel <artemsmotritel@gmail.com>
2022-06-05 22:37:48 +0300

    added flow, and, or functions

--- functions.js
+++ functions.js
@@ -31,10 +31,6 @@ const hasColor = (color) => (figure) => figure.color === color;
 const isRed = hasColor("red");
 const isBlue = hasColor("blue");
 
-function map(params) {}
-
-function filter(params) {}
-
 function reduce(cb, arr, startValue) {
   let result = startValue;
   arr.forEach((element) => {
@@ -63,3 +59,18 @@ function filter(cb, arr) {
 
   return result;
 }
+
+function flow(startValue, ...funcs) {
+  return funcs.reduce((res, func) => {
+    return func(res);
+  }, startValue);
+}
+
+const add1 = (x) => x + 1;
+const add5 = (x) => x + 5;
+const add10 = (x) => x + 10;
+
+console.log(flow(10, add1, add10, add5, add10));
+
+const and = (f1, f2) => (x) => f1(x) && f2(x);
+const or = (f1, f2) => (x) => f1(x) || f2(x);
